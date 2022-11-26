0a6935d2ea0458614297c1c16e4ad41f84f681a6
Vili Kinnunen <vili.kinnunen1@gmail.com>
2022-11-26 15:18:16 +0200

    Add TODO comment to make DynamoDb adapters suspend

--- app/src/main/kotlin/link/hattivatti/app/electricity/adapter/dynamodb/ElectricityPricesDynamoDbAdapter.kt
+++ app/src/main/kotlin/link/hattivatti/app/electricity/adapter/dynamodb/ElectricityPricesDynamoDbAdapter.kt
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
 
+// TODO make this suspending
 @Component
 class ElectricityPricesDynamoDbAdapter(
     @Value("\${dynamodb.electricity-prices.table-name}")
